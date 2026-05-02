3d9d574b28caf85e32c3fa70baf563d0a50ef295
saicharan910 <thecharan122004@gmail.com>
2026-05-02 20:22:02 +0530

    Add Customer model with basic fields

---
+++ delivery/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+
+# Create your models here.
+class Customer(models.Model):
+    username = models.CharField(max_length=20)
+    password = models.CharField(max_length=20)
+    email = models.CharField(max_length=20)
+    mobile = models.CharField(max_length=10)
+    address = models.CharField(max_length=50)
+
+
