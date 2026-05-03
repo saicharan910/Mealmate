7160cb8907aca5042d4c0fee623b4a095d1f2343
saicharan910 <thecharan122004@gmail.com>
2026-05-03 10:14:44 +0530

    Register Order and Activity models in admin

--- delivery/admin.py
+++ delivery/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 
-from .models import Customer
+from .models import Customer, Order, Activity
 
 # Register your models here.
 admin.site.register(Customer)
+admin.site.register(Order)
+admin.site.register(Activity)
