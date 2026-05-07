a85327ee2d021dcf1adb1c03a1f7ef0c23d1325f
saicharan910 <thecharan122004@gmail.com>
2026-05-07 14:38:09 +0530

    Delete delivery directory

--- delivery/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-
-from .models import Customer, Order, Activity
-
-# Register your models here.
-admin.site.register(Customer)
-admin.site.register(Order)
-admin.site.register(Activity)
